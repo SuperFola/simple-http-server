546214e2f36549faf56adc87112e25c7542c2bfd
SuperFola <lexplt.dev@gmail.com>
2020-11-22 18:33:45 +0100

    python2 inheritance is very different from python3

--- entrypoint.py
+++ entrypoint.py
@@ -10,16 +10,13 @@ PORT = 8080
 
 class RequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
     def __init__(self, request, client_addr, server):
-        super().__init__(request, client_addr, server)
+        SimpleHTTPServer.SimpleHTTPRequestHandler.__init__(self, request, client_addr, server)
 
     def block(self):
         print "rejected"
         self.send_response(403)
         self.end_headers()
         self.wfile.write("No.")
-
-    def do_HEAD(self):
-        super(RequestHandler, self).do_HEAD()
 
     def do_GET(self):
         parsed = urlparse.urlparse(self.path)
@@ -31,7 +28,7 @@ class RequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
         if any(s in parsed for s in forbidden):
             self.block()
         else:
-            super(RequestHandler, self).do_GET()
+            SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
 
     def do_POST(self):
         self.block()
